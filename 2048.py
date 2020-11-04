c4cb5fb7b6e74e2ccf7134eb762c62c9c87d2d2c
Ameya Laad <ameyalaad100@gmail.com>
2020-11-04 22:37:59 +0530

    Work update

---
+++ 2048.py
@@ -0,0 +1,7 @@
+# Game class
+# On arrow key -> perfoem move
+# GUI implementation -> update GUI after every move?
+# Storage Class
+# getstate, getpreviousstate
+# Move Class
+# left, right, up, down (inputs state)
