915583eedf6ca480d2e62db93c0535b19f189ba6
Ameya Laad <ameyalaad100@gmail.com>
2020-11-04 22:49:36 +0530

    To-do updated

--- 2048.py
+++ 2048.py
@@ -1,7 +1,13 @@
 # Game class
+# max score, Storage class, GUI(if needed)
 # On arrow key -> perfoem move
-# GUI implementation -> update GUI after every move?
+# init function -> 2x generate random -> update gui/ print current state
+# waitformove -> perform move -> update score -> check max score
+# update GUI/ print current state
+
 # Storage Class
-# getstate, getpreviousstate
+# current score, state, previous state
+# getstate, getpreviousstate, generaterandom, setpreviousstate, setstate, getscore, setscore
+
 # Move Class
-# left, right, up, down (inputs state)
+# left, right, up, down (inputs storage class), undo
