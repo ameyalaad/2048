0f546013c880ec06e4c6191b8840b0065da247fd
Nandish <nandishchheda301@gmail.com>
2020-11-19 12:26:53 +0530

    possible outcomes

--- 2048.py
+++ 2048.py
@@ -73,14 +73,27 @@ class Game:
             print(f"Anything else exits...\nScore = {self.storage.get_score()}, High Score = {self.max_score}")
             move = getch()
 
+            # fetch the possible outcomes before hand
+            scores = self.storage.generate_moves()
+            score_val = -1
+
+            #set the current state based on user input
             if move == b'w':
-                score_val = Move.up(self.storage)
+                if scores[0]!=-1:
+                    self.storage.set_state(self.storage.get_up_state())
+                    score_val = scores[0]
             elif move == b'a':
-                score_val = Move.left(self.storage)
+                if scores[1]!=-1:
+                    self.storage.set_state(self.storage.get_left_state())
+                    score_val = scores[1]
             elif move == b"s":
-                score_val = Move.down(self.storage)
+                if scores[2]!=-1:
+                    self.storage.set_state(self.storage.get_down_state())
+                    score_val = scores[2]
             elif move == b"d":
-                score_val = Move.right(self.storage)
+                if scores[3]!=-1:
+                    self.storage.set_state(self.storage.get_right_state())
+                    score_val = scores[3]
             elif move == b"n":
                 self.new_game()
             else:
@@ -89,9 +102,9 @@ class Game:
 
             # TODO: Implement Undo functionality
             # TODO: Possible combinations
+
             if score_val == -1:
                 print(f"No changes, try another move")
-
             else:
                 self.storage.generate_update()
                 new_score = self.storage.get_score() + score_val
@@ -111,10 +124,17 @@ class Game:
                     break    
 
     def check_game_over(self):
-        if self.storage.max_tile == 2048:
+        if max(self.storage.get_state()) == 2048:
             print(f"Congratulations!! You Won!\nScore = {self.storage.get_score()}, High Score = {self.max_score}")
             return True
-        if self.storage.num_empty_tiles == 0:
+
+        num_empty_tiles = 0
+        for i in range(4):
+            for j in range(4):
+                if self.storage.state[i][j] == -1:
+                    num_empty_tiles += 1
+
+        if num_empty_tiles == 0:
             # checking for merging tiles
             # temporary soln until lookahead is implemented
             for i in range(3): 
@@ -130,7 +150,10 @@ class Game:
                 if(self.storage.state[i][3]== self.storage.state[i + 1][3]): 
                     return False
 
+            os.system('cls' if os.name == 'nt' else 'clear')
             print("Game Over :(")
+
+            self.storage.show_state()
             return True
 
         return False
@@ -148,8 +171,10 @@ class Storage:
         self.score = 0
         self.state = [[-1 for _ in range(4)] for _ in range(4)]
         self.previous_state = None
-        self.max_tile = -1
-        self.num_empty_tiles = 16
+        self.left_state = None
+        self.right_state = None
+        self.up_state = None
+        self.down_state = None
 
     def get_state(self):
         return self.state
@@ -157,32 +182,41 @@ class Storage:
     def set_state(self, new_state):
         self.state = new_state
 
-    def get_max_tile(self):
-        return self.max_tile
-
-    def set_max_tile(self, new_max_tile):
-        self.max_tile = new_max_tile
-
     def get_previous_state(self):
         return self.previous_state
 
     def set_previous_state(self, new_previous_state):
         self.previous_state = new_previous_state
 
+    def get_left_state(self):
+        return self.left_state
+
+    def set_left_state(self, new_left_state):
+        self.left_state = new_left_state
+
+    def get_right_state(self):
+        return self.right_state
+
+    def set_right_state(self, new_right_state):
+        self.right_state = new_right_state   
+
+    def get_up_state(self):
+        return self.up_state
+
+    def set_up_state(self, new_up_state):
+        self.up_state = new_up_state
+
+    def get_down_state(self):
+        return self.down_state
+
+    def set_down_state(self, new_down_state):
+        self.down_state = new_down_state             
+
     def get_score(self):
         return self.score
 
     def set_score(self, new_score):
         self.score = new_score
-
-    def get_num_empty_tiles(self):
-        return self.num_empty_tiles
-
-    def increament_empty_tiles(self):
-        self.num_empty_tiles += 1
-
-    def decreament_empty_tiles(self):
-        self.num_empty_tiles -= 1
 
     def generate_update(self):
         """
@@ -192,20 +226,42 @@ class Storage:
         tile_value = random.choice([2, 2, 2, 2, 2, 2, 2, 2, 2, 4])
 
         # Get -1 elements as indices
-        random_empty_tile = random.randint(1, self.get_num_empty_tiles())
-
+        empty_indices = []
         for i in range(4):
             for j in range(4):
                 if self.state[i][j] == -1:
-                    random_empty_tile -= 1
-                if random_empty_tile == 0:
-                    self.state[i][j] = tile_value
-                    self.decreament_empty_tiles()
-                    break
-            if random_empty_tile == 0:
-                break
+                    empty_indices.append(i*4+j)
 
+        try:
+            location = random.choice(empty_indices)
+        except IndexError as error:
+            print("No empty tiles available. Game should now end")
+            # Implement move, will check the previous state comparision then
+            
+            return
+
+        self.state[location//4][location % 4] = tile_value       
         # print(self.state)
+
+    def generate_moves(self):
+        """
+        Generate the 4 possible outcome states
+        """
+        scores = [-1]*4
+        current_state = self.get_state()
+
+        scores[0], new_state = Move.up(current_state)
+        self.set_up_state(new_state)
+
+        scores[1], new_state = Move.left(current_state)
+        self.set_left_state(new_state)
+
+        scores[2], new_state = Move.down(current_state)
+        self.set_down_state(new_state)
+
+        scores[3], new_state = Move.right(current_state)
+        self.set_right_state(new_state)
+        return scores    
 
     def show_state(self):
         """Helper function to pretty-print the state"""
@@ -219,151 +275,87 @@ class Storage:
 
 
 # Move Class
-# left, right, up, down (inputs storage class), undo
+# left, right, up, down (input: state 2d-Array)
+# Utils: mirror, transpose, move_left
 class Move:
+
     @staticmethod
-    def left(storage):
-        storage.set_previous_state(deepcopy(storage.get_state()))
+    def left(state):
+        current_state = deepcopy(state)
+        valid_move, new_score, new_state = Move.move_left(current_state)
+        return (new_score if valid_move else -1), new_state
+
+    @staticmethod
+    def right(state): 
+        """ mirror -> left move """
+        current_state = deepcopy(state)
+        current_state = Move.mirror(current_state)
+        valid_move, new_score, new_state =  Move.move_left(current_state)
+        new_state = Move.mirror(new_state)
+        return (new_score if valid_move else -1), new_state
+
+    @staticmethod
+    def up(state):
+        """ transpose -> left move """
+        current_state = deepcopy(state)
+        current_state = Move.transpose(current_state)
+        valid_move, new_score, new_state = Move.move_left(current_state)
+        new_state = Move.transpose(new_state)
+        return (new_score if valid_move else -1), new_state
+
+    @staticmethod
+    def down(state):
+        """ transpose -> mirror -> left move """
+        current_state = deepcopy(state)
+        current_state = Move.transpose(current_state)
+        current_state = Move.mirror(current_state)
+        valid_move, new_score, new_state = Move.move_left(current_state)
+        new_state = Move.mirror(new_state)
+        new_state = Move.transpose(new_state)
+        return (new_score if valid_move else -1), new_state
+
+    @staticmethod
+    def move_left(current_state):
+        new_state = [[-1]*4 for _ in range(4)]       
         score = 0  # change in score
         valid_move = False  # valid if atleast 1 shift operation takes place
-        max_tile = -1
+
         for i in range(0, 4):
             nullIndex = 0  # first null index from left
-            lastMerge = 0  # index of last merge operation
             for j in range(0, 4):
-                if storage.state[i][j] != -1:
+                if current_state[i][j] != -1:
                     if nullIndex >= 1:
                         # Merge Condition
-                        if storage.state[i][j] == storage.state[i][nullIndex-1] and nullIndex > lastMerge:
-                            storage.state[i][nullIndex-1] *= 2
-                            score += 2*storage.state[i][j]
-                            lastMerge = nullIndex
-                            max_tile = max(max_tile, storage.state[i][nullIndex-1])
-                            storage.increament_empty_tiles()
+                        if current_state[i][j] == new_state[i][nullIndex-1]:
+                            new_state[i][nullIndex-1] *= 2
+                            score += new_state[i][nullIndex-1]
                         else:
-                            storage.state[i][nullIndex] = storage.state[i][j]
+                            new_state[i][nullIndex] = current_state[i][j]
                             nullIndex += 1
                     else:
-                        storage.state[i][nullIndex] = storage.state[i][j]
+                        new_state[i][nullIndex] = current_state[i][j]
                         nullIndex += 1
 
                     # Condition for tile shifting
                     if j != nullIndex-1:
-                        storage.state[i][j] = -1
                         valid_move = True
 
-        if max_tile > storage.get_max_tile():
-            storage.set_max_tile(max_tile)
-
-        return score if valid_move else -1
+        return valid_move, score, new_state    
 
     @staticmethod
-    def up(storage):
-        storage.set_previous_state(deepcopy(storage.get_state()))
-        score = 0  # change in score
-        valid_move = False  # valid if atleast 1 shift operation takes place
-        max_tile = -1
-        for i in range(0, 4):
-            nullIndex = 0  # first null index from top
-            lastMerge = 0  # index of last merge operation
-            for j in range(0, 4):
-                if storage.state[j][i] != -1:
-                    if nullIndex >= 1:
-                        # Merge Condition
-                        if storage.state[j][i] == storage.state[nullIndex-1][i] and nullIndex > lastMerge:
-                            storage.state[nullIndex-1][i] *= 2
-                            score += 2*storage.state[j][i]
-                            lastMerge = nullIndex
-                            max_tile = max(max_tile, storage.state[nullIndex-1][i])
-                            storage.increament_empty_tiles()
-                        else:
-                            storage.state[nullIndex][i] = storage.state[j][i]
-                            nullIndex += 1
-                    else:
-                        storage.state[nullIndex][i] = storage.state[j][i]
-                        nullIndex += 1
-
-                    # Condition for tile shifting
-                    if j != nullIndex-1:
-                        storage.state[j][i] = -1
-                        valid_move = True
-
-        if max_tile > storage.get_max_tile():
-            storage.set_max_tile(max_tile)
-
-        return score if valid_move else -1
-
+    def mirror(state):
+        for i in range(4):
+            state[i][0],state[i][3] = state[i][3], state[i][0]
+            state[i][1],state[i][2] = state[i][2], state[i][1]
+        return state    
+              
     @staticmethod
-    def right(storage):
-        storage.set_previous_state(deepcopy(storage.get_state()))
-        score = 0  # change in score
-        valid_move = False  # valid if atleast 1 shift operation takes place
-        max_tile = -1
-        for i in range(0, 4):
-            nullIndex = 3  # first null index from right
-            lastMerge = 3  # index of last merge operation
-            for j in reversed(range(0, 4)):
-                if storage.state[i][j] != -1:
-                    if nullIndex <= 2:
-                        # Merge Condition
-                        if storage.state[i][j] == storage.state[i][nullIndex+1] and nullIndex < lastMerge:
-                            storage.state[i][nullIndex+1] *= 2
-                            score += 2*storage.state[i][j]
-                            lastMerge = nullIndex
-                            max_tile = max(max_tile, storage.state[i][nullIndex+1])
-                            storage.increament_empty_tiles()
-                        else:
-                            storage.state[i][nullIndex] = storage.state[i][j]
-                            nullIndex -= 1
-                    else:
-                        storage.state[i][nullIndex] = storage.state[i][j]
-                        nullIndex -= 1
-
-                    # If tiles were shifted mark original positions as -1
-                    if j != nullIndex+1:
-                        storage.state[i][j] = -1
-                        valid_move = True
-
-        if max_tile > storage.get_max_tile():
-            storage.set_max_tile(max_tile)
-
-        return score if valid_move else -1
-
-    @staticmethod
-    def down(storage):
-        storage.set_previous_state(deepcopy(storage.get_state()))
-        score = 0  # change in score
-        valid_move = False  # valid if atleast 1 shift operation takes place
-        max_tile = -1
-        for i in range(0, 4):
-            nullIndex = 3  # first null index from bottom
-            lastMerge = 3  # index of last merge operation
-            for j in reversed(range(0, 4)):
-                if storage.state[j][i] != -1:
-                    if nullIndex <= 2:
-                        # Merge Condition
-                        if storage.state[j][i] == storage.state[nullIndex+1][i] and nullIndex < lastMerge:
-                            storage.state[nullIndex+1][i] *= 2
-                            score += 2*storage.state[j][i]
-                            lastMerge = nullIndex
-                            max_tile = max(max_tile, storage.state[nullIndex+1][i])
-                            storage.increament_empty_tiles()
-                        else:
-                            storage.state[nullIndex][i] = storage.state[j][i]
-                            nullIndex -= 1
-                    else:
-                        storage.state[nullIndex][i] = storage.state[j][i]
-                        nullIndex -= 1
-
-                    # Condition for tile shifting
-                    if j != nullIndex+1:
-                        storage.state[j][i] = -1
-                        valid_move = True
-
-        if max_tile > storage.get_max_tile():
-            storage.set_max_tile(max_tile)
-
-        return score if valid_move else -1
+    def transpose(state):
+        new_mat = [] 
+        for i in range(4):
+            for j in range(i,4):
+                state[i][j], state[j][i] = state[j][i], state[i][j]
+        return state                                
 
 
 def test():
